883103062eabc45714877711265e81069bc4a1f6
mwbullen <mwbullen@gmail.com>
2015-10-11 16:56:30 -0400

    First Commit

---
+++ Assets/CubeMovement.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeMovement : MonoBehaviour {
+	Vector3 rotateAxisVector;
+	Vector3 rotateAroundAxisVector;
+
+	float rotateDegree;
+	float rotateDegreeCounter;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ( rotateDegreeCounter > 0) {
+
+			transform.RotateAround (rotateAxisVector, rotateAroundAxisVector, rotateDegree * Time.deltaTime);
+			rotateDegreeCounter -= Mathf.Abs( (rotateDegree * Time.deltaTime));
+		}
+	}
+
+
+	public void RotateRight() {
+		if (rotateDegreeCounter <= 0) {
+			rotateAroundAxisVector = new Vector3 (0, 0, 1);
+			rotateAxisVector = transform.position + new Vector3 (.5f, -.5f, .5f);
+			rotateDegree = -90f;
+			rotateDegreeCounter = Mathf.Abs (rotateDegree);	
+		}
+	}
+
+	public void RotateLeft() {
+		if (rotateDegreeCounter <= 0) {
+			rotateAroundAxisVector = new Vector3 (0, 0, 1);
+			rotateAxisVector = transform.position + new Vector3 (-.5f, -.5f, .5f);
+			rotateDegree = 90f;
+			rotateDegreeCounter = Mathf.Abs (rotateDegree);	
+		}
+	}
+
+	public void RotateForward() {
+		if (rotateDegreeCounter <= 0) {
+			rotateAroundAxisVector = new Vector3 (1, 0, 0);
+			rotateAxisVector = transform.position + new Vector3 (0f, -.5f, .5f);
+			rotateDegree = 90f;
+			rotateDegreeCounter = Mathf.Abs (rotateDegree);	
+		}
+	}
+
+	public void RotateBack() {
+		if (rotateDegreeCounter <= 0) {
+			rotateAroundAxisVector = new Vector3 (1, 0, 0);
+			rotateAxisVector = transform.position + new Vector3 (0f, -.5f, -.5f);
+			rotateDegree = -90f;
+			rotateDegreeCounter = Mathf.Abs (rotateDegree);	
+		}
+	}
+
+
+}
